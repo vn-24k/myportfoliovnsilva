bb4d24299558aa6d9bbeedfc21cc898b1009b069
vn-24k <manuellesilva0305@gmail.com>
2026-04-26 23:40:07 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,85 @@
+<div class="hero-skills-area">
+  <div class="container">
+    <div class="row skills-grid">
+      
+      <div class="col-lg-3 col-md-4 col-sm-6 skill-item-wrapper">
+        <div class="skill-item glow-yellow">
+          <div class="skill-icon">
+            <img src="~/assets/images/python.png" alt="Python" />
+          </div>
+          <div class="skill-content">
+            <h5>Python</h5>
+            <p>(IA & Automação)</p>
+          </div>
+        </div>
+      </div>
 
-<script setup>
- 
-import Footer from "~/layouts/Footer.vue";
-import Header from "~/layouts/Header.vue";
+      <div class="col-lg-3 col-md-4 col-sm-6 skill-item-wrapper">
+        <div class="skill-item glow-orange">
+          <div class="skill-icon">
+            <img src="~/assets/images/aws.png" alt="AWS" />
+          </div>
+          <div class="skill-content">
+            <h5>AWS / Cloud</h5>
+          </div>
+        </div>
+      </div>
 
-useHead({
-  title: "Bentos - Personal Portfolio Nuxt js Template",
-});
+      <div class="col-lg-3 col-md-4 col-sm-6 skill-item-wrapper">
+        <div class="skill-item glow-blue">
+          <div class="skill-icon">
+            <img src="~/assets/images/docker.png" alt="Docker" />
+          </div>
+          <div class="skill-content">
+            <h5>Docker</h5>
+          </div>
+        </div>
+      </div>
 
+      <div class="col-lg-3 col-md-4 col-sm-6 skill-item-wrapper">
+        <div class="skill-item glow-cyan">
+          <div class="skill-icon">
+            <img src="~/assets/images/react.png" alt="ReactJS" />
+          </div>
+          <div class="skill-content">
+            <h5>ReactJS</h5>
+          </div>
+        </div>
+      </div>
 
-onMounted(async () => {
-  const wow = await import("wow.js");
-  new wow.default().init();
-});
- 
+      <div class="col-lg-3 col-md-4 col-sm-6 skill-item-wrapper">
+        <div class="skill-item glow-green">
+          <div class="skill-icon">
+            <img src="~/assets/images/node.png" alt="NodeJS" />
+          </div>
+          <div class="skill-content">
+            <h5>Node JS</h5>
+          </div>
+        </div>
+      </div>
 
-</script>
+      <div class="col-lg-3 col-md-4 col-sm-6 skill-item-wrapper">
+        <div class="skill-item glow-lightblue">
+          <div class="skill-icon">
+            <img src="~/assets/images/tailwind.png" alt="Tailwind CSS" />
+          </div>
+          <div class="skill-content">
+            <h5>Tailwind CSS</h5>
+          </div>
+        </div>
+      </div>
 
-<template>
-  <div>
-    <CommonScrollTop />
-    <Header /> 
-    <HomeAboutArea />
-    <HomePortfolioArea />
-    <HomeCtaArea /> 
-    <Footer />
+      <div class="col-lg-3 col-md-4 col-sm-6 skill-item-wrapper">
+        <div class="skill-item glow-brightgreen">
+          <div class="skill-icon">
+            <img src="~/assets/images/supabase.png" alt="Supabase" />
+          </div>
+          <div class="skill-content">
+            <h5>Supabase / SQL</h5>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
-</template>
-
-
- 
+</div>

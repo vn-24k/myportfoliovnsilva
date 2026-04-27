518acdb25b43d35149a80f7bc2fff45f77607fd5
vn-24k <manuellesilva0305@gmail.com>
2026-04-27 17:10:10 -0300

    Update Resume.vue

--- components/about/Resume.vue
+++ components/about/Resume.vue
@@ -12,12 +12,21 @@
                   <div class="content">
                     <span class="years">2025 - Presente</span>
                     <h4>Engenheiro de IA & LLMs</h4>
-                    <span class="company">Tech Lab Solutions</span>
+                    <span class="company">Projetos Independentes & Consultoria</span>
+                  </div>
+                </div>
+                <div class="resume-item wow fadeInUp delay-0-4s">
+                  <div class="icon"><i class="ri-code-s-slash-line"></i></div>
+                  <div class="content">
+                    <span class="years">2024 - 2025</span>
+                    <h4>Desenvolvedor Backend & Automação</h4>
+                    <span class="company">Soluções Digitais</span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+
           <div class="col-xl-6 col-md-6">
             <div class="single-resume">
               <h2>Educação</h2>
@@ -28,6 +37,14 @@
                     <span class="years">2025 - 2028</span>
                     <h4>Engenharia de Software</h4>
                     <span class="company">Universidade Tecnológica</span>
+                  </div>
+                </div>
+                <div class="resume-item wow fadeInUp delay-0-4s">
+                  <div class="icon"><i class="ri-brain-line"></i></div>
+                  <div class="content">
+                    <span class="years">Certificação</span>
+                    <h4>Especialista em Data Science & IA</h4>
+                    <span class="company">Plataforma Especializada</span>
                   </div>
                 </div>
               </div>

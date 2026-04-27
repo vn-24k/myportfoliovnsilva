52a2904c4d60fa086cd66a2304c495e3437a8012
vn-24k <manuellesilva0305@gmail.com>
2026-04-26 22:10:16 -0300

    Update Resume.vue

--- components/about/Resume.vue
+++ components/about/Resume.vue
@@ -1,6 +1,3 @@
-
-<template>
-  <section id="resume" class="resume-area">
 <template>
   <section id="resume" class="resume-area">
     <div class="container">
@@ -18,26 +15,9 @@
                     <span class="company">Tech Lab Solutions</span>
                   </div>
                 </div>
-                <div class="resume-item wow fadeInUp delay-0-3s">
-                  <div class="icon"><i class="ri-code-box-line"></i></div>
-                  <div class="content">
-                    <span class="years">2023 - 2025</span>
-                    <h4>Desenvolvedor Backend Sênior</h4>
-                    <span class="company">Cloud Systems Cor</span>
-                  </div>
-                </div>
-                <div class="resume-item wow fadeInUp delay-0-3s">
-                  <div class="icon"><i class="ri-global-line"></i></div>
-                  <div class="content">
-                    <span class="years">2021 - 2023</span>
-                    <h4>Engenheiro de Software Fullstack</h4>
-                    <span class="company">DevPulse Digital</span>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
-          
           <div class="col-xl-6 col-md-6">
             <div class="single-resume">
               <h2>Educação</h2>
@@ -48,22 +28,6 @@
                     <span class="years">2025 - 2028</span>
                     <h4>Engenharia de Software</h4>
                     <span class="company">Universidade Tecnológica</span>
-                  </div>
-                </div>
-                <div class="resume-item wow fadeInUp delay-0-3s">
-                  <div class="icon"><i class="ri-brain-line"></i></div>
-                  <div class="content">
-                    <span class="years">2026</span>
-                    <h4>IA & Ciência de Dados</h4>
-                    <span class="company">Advanced Data Institute</span>
-                  </div>
-                </div>
-                <div class="resume-item wow fadeInUp delay-0-3s">
-                  <div class="icon"><i class="ri-terminal-line"></i></div>
-                  <div class="content">
-                    <span class="years">2024 - 2025</span>
-                    <h4>Arquitetura de Sistemas</h4>
-                    <span class="company">Tech Academy</span>
                   </div>
                 </div>
               </div>

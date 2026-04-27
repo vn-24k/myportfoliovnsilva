01a117592f6fbc49bb1c85e017663568831bc72a
vn-24k <manuellesilva0305@gmail.com>
2026-04-27 12:46:52 -0300

    Update About.vue

--- components/about/About.vue
+++ components/about/About.vue
@@ -1,32 +1,44 @@
+
 <template>
   <section id="about" class="about-single-area innerpage-single-area">
     <div class="container">
       <div class="row">
+        <!-- START ABOUT IMAGE DESIGN AREA -->
         <div class="col-lg-4">
           <div class="about-image-part wow fadeInUp delay-0-3s">
-            <img src="~/assets/images/about/profile.png" alt="Vinícius Silva" />
+            <img src="~/assets/images/about/profile.png" alt="About Me" />
           </div>
         </div>
+        <!-- / END ABOUT IMAGE DESIGN AREA -->
+        <!-- START ABOUT TEXT DESIGN AREA -->
         <div class="col-lg-8">
           <div class="about-content-part wow fadeInUp delay-0-2s">
-            <h2> Vinícius Silva | Engenheiro de Software.</h2>
+            <h2>{{"I’m"}} Bentos Walker, a product designer.</h2>
             <p>
-              Especialista em IA & Automação com foco em arquitetura avançada de sistemas multi-agentes (LLMs) e Python. 
-              Transformo infraestruturas complexas em ecossistemas autônomos e escaláveis.
+              I am a San francisco-based product designer with a focus on web
+              design, illustration, a visual development. I have a diverse range
+              of experience having worked across various fields and industries.
             </p>
             <p>
-              Minha trajetória inclui sólida experiência como Engenheiro Fullstack em sistemas críticos, 
-              utilizando C# e SQL Server para otimização de fluxos de dados massivos e implementação 
-              de soluções backend de alta performance.
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source.
             </p>
             <div class="hero-btns">
               <NuxtLink to="/contact" class="theme-btn"
-                >Vamos Conversar<i class="ri-mail-send-line"></i
+                >Get In touch<i class="ri-mail-send-line"></i
               ></NuxtLink>
             </div>
           </div>
         </div>
-        </div>
+        <!-- / END ABOUT TEXT DESIGN AREA -->
+      </div>
     </div>
   </section>
 </template>
+
+ 
